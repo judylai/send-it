57fc2a5dd44ba60bb2197503a8a0f6639b9b06c2
judylai <juudylai@gmail.com>
2019-09-04 16:30:05 -0700

    Update Header.js
    Changed subheader

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <React.Fragment>
       <Jumbotron>
         <h1>Shopping List</h1>
-        <p>Keep track of what matters most -- the stuff going in your belly next.</p>
+        <p>Keep track of your next climb.</p>
       </Jumbotron>
     </React.Fragment>
   );
